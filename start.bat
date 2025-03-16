63b1a33752909c4cc5ca9824b944c1a9014d0903
erikwhisper <erlud100@uni-duesseldorf.de>
2025-03-16 20:06:08 +0100

    added errorlevel to start.bat

--- start.bat
+++ start.bat
@@ -3,3 +3,6 @@ echo Starting server on http://localhost:8080
 cd gui
 start "" http://localhost:8080/templates/index.html
 python -m http.server 8080
+if %errorlevel% neq 0 (
+    py -m http.server 8080
+)
